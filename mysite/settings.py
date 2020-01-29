cb2a5d48b754666961bfa810e885edb6ac7596a6
Eastrong <tmp100500@mail.ru>
2020-01-29 17:56:54 +1000

    added some stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%*j_^jy+4yf&kx7aox+7f991ky$s*py!e%)s%#^h35w#wtmh$#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
